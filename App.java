91ecec3fb3d83cce8f1754c97bb3d93dd6ba5d05
root <root@jenkins-devops-vm.us-central1-f.c.kkgcplabs01-043.internal>
2026-01-15 18:39:53 +0000

    Add Java main class

---
+++ App.java
@@ -0,0 +1,14 @@
+package com.devops;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java DevOps App from humara khan");
+        while (true) {
+            try {
+                Thread.sleep(10000);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
+
