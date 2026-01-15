755a1caf9c6b8b10f5e07c8fca9dda8b5a450257
mustaqim12345 <mustaqim8859@gmail.com>
2026-01-16 00:26:43 +0530

    Update App.java

--- App.java
+++ App.java
@@ -4,10 +4,11 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Java App Started");
 
-        while (true) {
+        // Keep app running so Docker container doesn't exit
+        while(true){
             try {
                 Thread.sleep(10000);
-            } catch (Exception e) {}
+            } catch(Exception e){}
         }
     }
 }
